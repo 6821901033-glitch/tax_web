36419a953565b9f3f384c1c22193ad3988bf950d
6821901033-glitch <6821901033@cdti.ac.th>
2026-08-26 09:49:27 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import BlogCard from "@/components/BlogCard";
 
 export default function Home() {
   return (<div>
       <h1>Welcome to my App</h1>
-      
+      <BlogCard/>
     </div>
   );
 }
